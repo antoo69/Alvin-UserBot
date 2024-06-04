a3197cd4db20127e6165e92af5c1385bace22dd3
Alvin Junior <88810688+fahrial2310@users.noreply.github.com>
2024-06-04 22:12:38 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+import os, sys
+os.system("pkg install pip")
+os.system("pip install telethon")
+os.system("pip install asyncio")
+os.system("pip install datetime")
+os.system("pip install --upgrade pip setuptools")
+os.system("pip install -r Requirements.txt")
+os.system("clear")
